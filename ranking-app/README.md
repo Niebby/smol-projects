cffc29aac9341affada8d4a33bf56091323b6bf6
Niebby <mateuszdrabek99@gmail.com>
2022-12-27 21:24:02 +0100

    Added saving results to file

--- ranking-app/README.md
+++ ranking-app/README.md
@@ -3,12 +3,11 @@ Short Python script that takes entries from a file, pairs all of them and asks t
 
 Right now it matches each entry against all other entries, resulting in the match count being the *n*-th triangular number with the number of entries being *n+1*. This makes the number of choices the user has to make unreasonably high for higher numbers of entries - for example with 5 entries there are 10 choices to make, but having 15 choices you need to resolve 105 match-ups.
 
+The results will be printed to standard output and to a file "out-file.txt" in the directory the script is located in. **The output file will be overwritten.** 
+
 ### TODO (may or may not ever implement)
-* Printing the results to file
 * Option to reduce the number of matches in a fair way
 * Make ties take the same place in ranking
 * Option to go back and change an answer 
 * Pretty up the prompts
 * Add usage description to this README
-
-
--- ranking-app/ranking.py
+++ ranking-app/ranking.py
@@ -3,30 +3,35 @@ from random import shuffle
 import sys
 
 with open("in-file.txt", "r") as infile:
-    # infile = ["a", "b", "c", "d"]
+    # infile = ["borgar", "hotdog", "kebab", "taco"]
     contenders = {k.rstrip():0 for k in infile}
-    print(contenders)
-    pairings = list(combinations(contenders.keys(), 2))
-    shuffle(pairings)
-    print(pairings)
-    for num, i in enumerate(pairings):
-        print()
-        print("\n\n-----   " + i[0] + "   VS   " + i[1] + "   ----- (" + str(num+1) + "/" + str(len(pairings)) + ")")
-        print("Choose 1 to vote for " + i[0])
-        print("Choose 2 to vote for " + i[1])
-        user_choice = None
-        while user_choice not in [1, 2]:
-            try:
-                user_choice = int(input().strip())
-            except KeyboardInterrupt:
-                sys.exit()
-            except:
-                pass
-        print("Voted for " + i[user_choice-1])
-        contenders[i[user_choice-1]] += 1
+
+# print(contenders)
+pairings = list(combinations(contenders.keys(), 2))
+shuffle(pairings)
+# print(pairings)
+for i, matchup in enumerate(pairings):
+    print()
+    print("\n\n-----   " + matchup[0] + "   VS   " + matchup[1] + "   ----- (" + str(i+1) + "/" + str(len(pairings)) + ")")
+    print("Choose 1 to vote for " + matchup[0])
+    print("Choose 2 to vote for " + matchup[1])
+    user_choice = None
+    while user_choice not in [1, 2]:
+        try:
+            user_choice = int(input().strip())
+        except KeyboardInterrupt:
+            sys.exit()
+        except:
+            pass
+    print("Voted for " + matchup[user_choice-1])
+    contenders[matchup[user_choice-1]] += 1
         
+with open("out-file.txt", "w") as outfile:
     print("\n\n\n")
     placement = 1
     for k, v in sorted(contenders.items(), key=lambda k: -k[1]):
-        print(str(placement) + ".", k + "   ("+ str(v) +  " wins - " + "{:.2f}".format(100*((len(contenders.keys())-1) and v / (len(contenders.keys())-1) or 0)) + "% won)")
+        print_string = str(placement) + ". " + k + "   ("+ str(v) +  " wins - " + "{:.2f}".format(100*((len(contenders.keys())-1) and v / (len(contenders.keys())-1) or 0)) + "% won)\n"
+        outfile.write(print_string)
+        print(print_string, end='')
+        
         placement += 1
